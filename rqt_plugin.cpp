8e1326cf86badfc141f7709ce63907a20860918a
shiaoming zhao <9512617+Shiaoming@users.noreply.github.com>
2019-04-24 17:01:23 +0800

    add necessary comments to help coding

--- rqt_plugin.cpp
+++ rqt_plugin.cpp
@@ -9,6 +9,7 @@ namespace rqt_%{Plugin} {
 %{CN}::%{CN}() :
     rqt_gui_cpp::Plugin()
 {
+    // Constructor is called first before initPlugin function, needless to say.
     setObjectName("%{CN}");
 }
 
@@ -20,17 +21,19 @@ void rqt_%{Plugin}::%{CN}::initPlugin(qt_gui_cpp::PluginContext &context)
 
 void rqt_%{Plugin}::%{CN}::shutdownPlugin()
 {
-
+    // TODO unregister all publishers here
 }
 
 void rqt_%{Plugin}::%{CN}::saveSettings(qt_gui_cpp::Settings &plugin_settings, qt_gui_cpp::Settings &instance_settings) const
 {
-
+    // TODO save intrinsic configuration, usually using:
+    // instance_settings.setValue(k, v)
 }
 
 void rqt_%{Plugin}::%{CN}::restoreSettings(const qt_gui_cpp::Settings &plugin_settings, const qt_gui_cpp::Settings &instance_settings)
 {
-
+    // TODO restore intrinsic configuration, usually using:
+    // v = instance_settings.value(k)
 }
 
 } // end namespace rqt_%{Plugin}
